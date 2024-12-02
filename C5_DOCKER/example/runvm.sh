32d638b8de73bf0d5cf769070f033dbf1500379f
Jean-Baptiste BESNARD <jbbesnard@paratools.fr>
2024-12-02 09:53:16 +0100

    VM run example

---
+++ C5_DOCKER/example/runvm.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#qemu-img create -f qcow2 ./mydebian.qcow2 10G
+
+qemu-system-x86_64 -cdrom ./debian-11.2.0-amd64-netinst.iso -hda ./mydebian.qcow2 -m 1024 -netdev user,id=eth0,hostfwd=tcp::10022-:22 -device e1000,netdev=eth0
+
+
