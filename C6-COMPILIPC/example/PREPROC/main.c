4a4f26b7e9cda0e9357015c3c093580345364a04
Jean-Baptiste BESNARD <jbbesnard@paratools.fr>
2024-12-03 10:32:14 +0100

    More macros

--- C6-COMPILIPC/example/PREPROC/main.c
+++ C6-COMPILIPC/example/PREPROC/main.c
@@ -17,6 +17,13 @@ static char * MSG = "PAS CONTENT";
 #define str(s) #s
 #define HELLO xstr(H)
 
+#if __STDC__
+#warning "STDC=1"
+#else
+#warning "STDC=0"
+#endif
+
+
 #define TOTO 5
 
 char* foo()
@@ -32,9 +39,19 @@ char* foo2()
 }
 
 
+#define log(...) do{\
+			char buff[128];\  
+			snprintf (buff, 128, __VA_ARGS__);\
+			fprintf(stderr, "%s:%d >>> %s", __FILE__, __LINE__, buff);\
+		}while(0)
+
 
 int main(int argc, char ** argv)
 {
-	printf("%s je suis %s %s %s\n", HELLO, MSG, foo(), foo2());
+	log("%s je suis %s %s %s\n", HELLO, MSG, foo(), foo2());
+
+	printf("%s : %d\n", __FILE__, __LINE__);
+	printf("%s %s\n", __DATE__, __TIME__);
+
 	return 0;
 }
