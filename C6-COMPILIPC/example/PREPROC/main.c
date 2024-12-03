7825360f52a2f661152d9a74bb681d97bfd57408
Jean-Baptiste BESNARD <jbbesnard@paratools.fr>
2024-12-03 10:15:51 +0100

    More macros

--- C6-COMPILIPC/example/PREPROC/main.c
+++ C6-COMPILIPC/example/PREPROC/main.c
@@ -6,13 +6,35 @@ static char * MSG = "CONTENT";
 static char * MSG = "PAS CONTENT";
 #endif
 
+#ifdef CRASH
+	#error "Aie"
+#endif
+
+#warning "What is this ?"
+
 #define H Hello
 #define xstr(s) str(s)
 #define str(s) #s
 #define HELLO xstr(H)
 
+#define TOTO 5
+
+char* foo()
+{
+	return xstr(TOTO);
+}
+
+#undef TOTO
+
+char* foo2()
+{
+	return xstr(TOTO);
+}
+
+
+
 int main(int argc, char ** argv)
 {
-	printf("%s je suis %s\n", HELLO, MSG);
+	printf("%s je suis %s %s %s\n", HELLO, MSG, foo(), foo2());
 	return 0;
 }
